d23e072c8091a1beedf39121ad6ac6ccf21630c3
Billy Linder <contact@billylinder.com>
2023-07-17 10:03:47 -0400

    Initial commit

---
+++ gtm-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: GTM Plugin
+Plugin URI: https://github.com/billylinder/WordPress-GTM
+Description: A WordPress plugin to add Google Tag Manager (GTM) to your site.
+Version: 1.0
+Author: Billy Linder
+Author URI: https://www.billylinder.com
+*/
